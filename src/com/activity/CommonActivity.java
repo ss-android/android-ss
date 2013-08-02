03520e1729ca2ba09df345f5327f233bb9051bfa
retryu <ruanchenyugood@gmail.com>
2013-08-02 11:02:07 +0800

    init

--- src/com/activity/CommonActivity.java
+++ src/com/activity/CommonActivity.java
@@ -8,7 +8,7 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.example.sansheng.R;
 
-//09:17
+//init  ok
 public class CommonActivity extends SherlockFragmentActivity {
 	public static int THEME = R.style.Theme_Sherlock_Light;
 	TextView tvTitle;
